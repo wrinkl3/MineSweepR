58bd8421b6eaef8e3aa48eb6c90539bb5b23adc2
wrinkl3 <al.seward@gmail.com>
2017-10-16 18:13:11 +0400

    Add outlier detection

--- stat.py
+++ stat.py
@@ -1,19 +1,21 @@
-import psutil
-import time
+import psutil, time
 from selenium import webdriver
+from utils import upper_outlier_indices
+from operator import itemgetter
 
 
 class SiteStater:
-    baseline_website = 'http://whatismyip.akamai.com'  # ეს უფრო სწრაფად იტვირთება ვიდრე Google
+    baseline_website = 'http://whatismyip.akamai.com'
 
     @staticmethod
     def stat_current():
         return psutil.cpu_percent(interval=None)
 
     def stat_website(self, site_url):
+        time.sleep(5)
         self.stat_current()
         self.driver.get(site_url)
-        time.sleep(5)  # CPU ბოლომდე რომ აიწიოს
+        time.sleep(2)  # CPU ბოლომდე რომ აიწიოს
         after = self.stat_current()
         self.driver.get(self.baseline_website)  # reset
 
@@ -29,9 +31,10 @@ class SiteStater:
 
 def main():
     with open("./urls.list", "rb") as f:
-        urls = f.readlines()
+        urls = [url.strip().decode() for url in f.readlines() if url.startswith(b'http')]
     stater = SiteStater()
 
+    '''
     for url in urls:
         url = url.strip().decode()
         if not url.startswith("http"):
@@ -39,7 +42,15 @@ def main():
         score = stater.stat_website(url)
         print('{} consumption at {}'.format(url, score))
     stater.close()
-
+    '''
+    scores = [stater.stat_website(url) for url in urls]
+    print(scores)
+    indices = upper_outlier_indices(scores, 0.5)
+    if len(indices)>0:
+        print('These websites might have a miner:')
+        print(itemgetter(*indices)(urls))
+    else:
+        print('No outliers detected')
 
 if __name__ == "__main__":
     main()
---
+++ utils.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def find_upper_outliers(data, m=1.5):
+    quartile_1, quartile_3 = np.percentile(data, [25, 75])
+    iqr = quartile_3 - quartile_1
+    lower_bound = quartile_1 - (iqr * m)
+    upper_bound = quartile_3 + (iqr * m)
+    print(upper_bound)
+    return np.where((data > upper_bound))
+
+def upper_outlier_indices(data, m=1.5):
+	return find_upper_outliers(data, m)[0].tolist()

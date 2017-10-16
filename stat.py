f5e678e9f577b94ff25d7f2ff3be72dcf4652f78
Lasha Khasaia <secrary@users.noreply.github.com>
2017-10-16 18:19:20 +0400

    Update stat.py

--- stat.py
+++ stat.py
@@ -15,7 +15,7 @@ class SiteStater:
         time.sleep(5)
         self.stat_current()
         self.driver.get(site_url)
-        time.sleep(2)  # CPU ბოლომდე რომ აიწიოს
+        time.sleep(2)
         after = self.stat_current()
         self.driver.get(self.baseline_website)  # reset
 
@@ -34,17 +34,8 @@ def main():
         urls = [url.strip().decode() for url in f.readlines() if url.startswith(b'http')]
     stater = SiteStater()
 
-    '''
-    for url in urls:
-        url = url.strip().decode()
-        if not url.startswith("http"):
-            continue
-        score = stater.stat_website(url)
-        print('{} consumption at {}'.format(url, score))
-    stater.close()
-    '''
     scores = [stater.stat_website(url) for url in urls]
-    print(scores)
+    # print(scores)
     indices = upper_outlier_indices(scores, 0.5)
     if len(indices)>0:
         print('These websites might have a miner:')

25f30d6fc316c3947560b387382ce5610461e17b
Lasha Khasaia <secrary@users.noreply.github.com>
2017-10-17 13:43:35 +0400

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def find_upper_outliers(data, m=1.5):
     quartile_1, quartile_3 = np.percentile(data, [25, 75])
     iqr = quartile_3 - quartile_1
@@ -7,5 +8,6 @@ def find_upper_outliers(data, m=1.5):
     upper_bound = quartile_3 + (iqr * m)
     return np.where((data > upper_bound))
 
+
 def upper_outlier_indices(data, m=1.5):
-	return find_upper_outliers(data, m)[0].tolist()
+    return find_upper_outliers(data, m)[0].tolist()

f73528c760e4e1b09546aeaeb9ff0e15f21e6e39
Lasha Khasaia <secrary@users.noreply.github.com>
2017-10-17 13:42:50 +0400

    Update site_stat.py

--- site_stat.py
+++ site_stat.py
@@ -1,40 +1,36 @@
 import psutil, time
 from selenium import webdriver
 
+
 class SiteStater:
-    baseline_website = 'http://whatismyip.akamai.com'
+    def go_to_baseline(self):
+        self.driver.get(self.baseline_website)
 
     def stat_current(self):
         return self.process.cpu_percent(interval=None)
 
     def stat_website(self, site_url):
+        self.init_driver()
+        self.go_to_baseline()
         self.stat_current()
         self.driver.get(site_url)
         time.sleep(2)
         after = self.stat_current()
-        #self.init_driver()
-        self.go_to_baseline()
+        self.close()
 
         return after
 
     def init_driver(self):
-        if hasattr(self, 'driver'):
-            self.close()
-            self.process = None
         self.driver = webdriver.PhantomJS()
         for child in psutil.Process().children():
-            if 'phantomjs' in child.name():
+            if 'phantom' in child.name():  # 'geckodriver' for firefox
                 self.process = child
                 break
-        if not hasattr(self, 'process') or self.process is None:
-            raise Error('Failed to launch PhantomJS')
+        if not self.process:
+            raise Exception('Failed to launch PhantomJS')
 
-    def go_to_baseline(self):
-        self.driver.get(self.baseline_website)
-    
     def __init__(self):
-        self.init_driver()
-        self.go_to_baseline() # initial
+        self.baseline_website = 'http://whatismyip.akamai.com'
 
     def close(self):
-        self.driver.close()
+        self.driver.quit()

056de92a6c2df0f0e673bfc8f794f2ac25d563d7
wrinkl3 <al.seward@gmail.com>
2017-10-18 11:01:54 +0400

    Add configurable arguments to the cli

--- site_stat.py
+++ site_stat.py
@@ -15,7 +15,7 @@ class SiteStater:
         self.stat_current()
         try:
             self.driver.get(site_url)
-            time.sleep(5)
+            time.sleep(self.pause)
             after = self.stat_current()
         except:
             after = None
@@ -25,7 +25,7 @@ class SiteStater:
 
     def init_driver(self):
         self.driver = webdriver.PhantomJS()
-        self.driver.set_page_load_timeout(60)
+        self.driver.set_page_load_timeout(self.timeout)
         for child in psutil.Process().children():
             if 'phantom' in child.name():  # 'geckodriver' for firefox
                 self.process = child
@@ -33,9 +33,11 @@ class SiteStater:
         if not self.process:
             raise Exception('Failed to launch PhantomJS')
 
-    def __init__(self):
+    def __init__(self, pause=10, timeout=60):
         self.driver = None
         self.process = None
+        self.pause = pause
+        self.timeout = timeout
         self.baseline_website = 'http://whatismyip.akamai.com'
 
     def close(self):
--- sweep.py
+++ sweep.py
@@ -11,14 +11,11 @@ def stat_and_print(url, stater):
         print("{}: {}".format(url, score))
     return score
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("urls", help="the file that contains the urls")
-    args = parser.parse_args()
 
-    with open(args.urls, "rb") as f:
+def run(urls, timeout, pause):
+    with open(urls, "rb") as f:
         urls = [url.strip().decode() for url in f.readlines() if url.startswith(b'http')]
-    stater = SiteStater()
+    stater = SiteStater(timeout=timeout, pause=pause)
 
     scores, valid_urls = [], []
     for url in urls:
@@ -26,13 +23,25 @@ def main():
         if score:
             scores.append(score)
             valid_urls.append(url)
-    
+
     indices = upper_outlier_indices(scores, 0.5)
     if len(indices) > 0:
         print('These websites might have a miner:')
         print(itemgetter(*indices)(valid_urls))
     else:
         print('No outliers detected')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("urls", help="the file that contains the urls")
+    parser.add_argument("-t", "--timeout", type=int, default=60,
+                    help="how much the browser should wait for the page before timing out")
+    parser.add_argument("-p", "--pause", type=int, default=10,
+                    help="how much the browser should stay on a page before measuring the cpu performance")
+
+    args = parser.parse_args()
+    run(args.urls, args.timeout, args.pause)
 
 
 if __name__ == "__main__":
